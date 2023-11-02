04198d6de64ce053e7b5a7b2e6f6514eeeb11193
WalkingWithSpacemen3 <Frakctures@outlook.com>
2023-11-02 21:31:38 +0100

    Fix formatting and update test cases

--- Lab3/test_list.cc
+++ Lab3/test_list.cc
@@ -1,13 +1,13 @@
-#include "catch.hpp"
+#include "../catch.hpp"
 #include "List.h"
-
 
 //=======================================================================
 // Test cases
 //=======================================================================
 
-TEST_CASE("Create list") {
-	List l{1,11,3,2,6,9,8,7};
+TEST_CASE("Create list")
+{
+	List l{1, 11, 3, 2, 6, 9, 8, 7};
 
 	CHECK(l.at(3) == 6);
 	CHECK(l.at(5) == 8);
@@ -17,16 +17,18 @@ TEST_CASE("Create list") {
 	CHECK(l.at(5) == 7);
 }
 
-TEST_CASE("REMOVE ELEMENTS") {
-	List l{ 1,11,3,2,6,9,8,7 };
+TEST_CASE("REMOVE ELEMENTS")
+{
+	List l{1, 11, 3, 2, 6, 9, 8, 7};
 	l.remove(7);
 	CHECK(l.at(5) == 9);
 	l.remove_at(1);
 	CHECK(l.at(1) == 3);
 }
 
-TEST_CASE("COPY AND TO_STRING") {
-	List l{ 2,5,1,3,8,4 };
-	List a{ l };
+TEST_CASE("COPY AND TO_STRING")
+{
+	List l{2, 5, 1, 3, 8, 4};
+	List a{l};
 	CHECK(l.to_string() == "1 2 3 4 5 8");
 }
