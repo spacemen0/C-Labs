371c9d1fab24927fb34fe89de83751bbdeae7991
spacemen0 <frakctures@outlook.com>
2023-12-05 21:31:57 +0100

    Update component names in Lab4/main.cpp

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -7,12 +7,12 @@ int main(int argc, char *argv[])
 {
     Connection p, l, r, n;
     Circuit net(200000, 10, 0.01);
-    net.addComponent(new Battery("bat", 24.0, p, n));
-    net.addComponent(new Resistor("r1", 150, p, l));
-    net.addComponent(new Resistor("r2", 50, p, r));
-    net.addComponent(new Capacitor("c3", 1.0, r, l));
-    net.addComponent(new Resistor("r4", 300, l, n));
-    net.addComponent(new Capacitor("c5", 0.75, r, n));
+    net.addComponent(new Battery("Bat", 24.0, p, n));
+    net.addComponent(new Resistor("R1", 150, p, l));
+    net.addComponent(new Resistor("R2", 50, p, r));
+    net.addComponent(new Capacitor("C3", 1.0, r, l));
+    net.addComponent(new Resistor("R4", 300, l, n));
+    net.addComponent(new Capacitor("C5", 0.75, r, n));
     net.printHeader();
     net.simulate();
     net.deallocate_components();
